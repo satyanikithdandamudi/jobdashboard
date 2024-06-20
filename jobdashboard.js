25841813661eacd8d036925d0155af69889b4ca1
Noctural93 <gowtham313754@gmail.com>
2024-06-20 18:45:55 +0530

    nav color

--- jobdashboard.js
+++ jobdashboard.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    $('.dropdown').hover(function() {
-        $(this).addClass('show');
-        $(this).find('.dropdown-menu').addClass('show');
-    }, function() {
-        $(this).removeClass('show');
-        $(this).find('.dropdown-menu').removeClass('show');
-    });
-});
+// $(document).ready(function() {
+    // $('.dropdown').hover(function() {
+        // $(this).addClass('show');
+        // $(this).find('.dropdown-menu').addClass('show');
+    // }, function() {
+        // $(this).removeClass('show');
+        // $(this).find('.dropdown-menu').removeClass('show');
+    // });
+// });
