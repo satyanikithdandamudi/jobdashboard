31d450a984d077fe999bfaa274e805d1ad21f107
satyanikithdandamudi <dsnd123456@gmail.com>
2024-06-21 20:09:28 +0530

    nav and scrolling

--- jobdashboard.js
+++ jobdashboard.js
@@ -7,3 +7,56 @@
         // $(this).find('.dropdown-menu').removeClass('show');
     // });
 // });
+
+// const infiteScrollData = [
+//     {
+//         "text": "Placed"
+//     },
+//     {
+//         "text": "placed"
+//     },
+//     {
+//         "text": "placed"
+//     },
+//     {
+//         "text": "placed"
+//     },
+//     {
+//         "text": "placed"
+//     },
+//     {
+//         "text": "placed"
+//     },
+//     {
+//         "text": "placed"
+//     },
+//     {
+//         "text": "placed"
+//     },
+//     {
+//         "text": "placed"
+//     },
+//     {
+//         "text": "placed"
+//     }
+// ]
+
+// const searchInputEl = document.querySelector('.search-input-el');
+
+// const infiniteScrollListItems = document.querySelectorAll('.infinite-scroll-list-items');
+
+// // infinite scroll
+
+// infiteScrollData.map((item, index) => {
+//     const infiniteScrolllistEl = document.createElement('li');
+//     infiniteScrolllistEl.textContent = `${item.text}`;
+//     infiniteScrolllistEl.classList.add('infinite-scroll-list');
+//     infiniteScrollListItems[0].appendChild(infiniteScrolllistEl)
+// });
+
+// infiteScrollData.map((item, index) => {
+//     const infiniteScrolllistEl = document.createElement('li');
+//     infiniteScrolllistEl.textContent = `${item.text}`;
+//     infiniteScrolllistEl.classList.add('infinite-scroll-list');
+//     infiniteScrollListItems[1].appendChild(infiniteScrolllistEl)
+// });
